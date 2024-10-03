24f77515d8c94020cca21c4cd7bcb5e8fbec1e4f
VadimNS <vadik1212a@gmail.com>
2024-10-03 17:27:17 +0300

    correction

--- app/components/DateInput.tsx
+++ app/components/DateInput.tsx
@@ -8,15 +8,33 @@ interface DateInputProps {
 }
 
 const DateInput: React.FC<DateInputProps> = ({ label, value, setValue, error }) => {
+  let placeholder = "";
+  let min = 1;
+  let max = 1;
+
+  if (label === "DAY") {
+    placeholder = "DD";
+    min = 1;
+    max = 31;
+  } else if (label === "MONTH") {
+    placeholder = "MM";
+    min = 1;
+    max = 12;
+  } else if (label === "YEAR") {
+    placeholder = "YYYY";
+    min = 1;
+    max = new Date().getFullYear(); 
+  }
+
   return (
     <div className="input-dates">
       <span className="input-nav">{label}</span>
       <input
         className="input-numbers"
         type="number"
-        placeholder={label === "DAY" ? "DD" : label === "MONTH" ? "MM" : "YYYY"}
-        min={label === "DAY" ? "1" : label === "MONTH" ? "1" : "1"}
-        max={label === "DAY" ? "31" : label === "MONTH" ? "12" : new Date().getFullYear().toString()}
+        placeholder={placeholder}
+        min={min}
+        max={max}
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
